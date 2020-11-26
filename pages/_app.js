dd797c0d704c9b6d3888faab6af2cde1e98c44a2
Ruben <r.vitt@fme.de>
2020-11-26 07:03:31 +0100

    :sparkles:(general): some additions

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,12 @@ import '../styles/globals.css'
 import Image from "next/image";
 import React from "react";
 import {useRouter} from "next/router";
-import {publicRoutes} from "../routes";
+import {allRoutes} from "../routes";
 import {PageContainer} from "../components/page-container.component";
 
 function MyApp({Component, pageProps}) {
     const {route} = useRouter();
-    const pageTitle = publicRoutes.find(r => r.path === route && !r.hideTitle)?.title ?? '';
+    const pageTitle = allRoutes.find(r => r.path === route && !r.hideTitle)?.title ?? '';
     return <>
         <PageContainer title={pageTitle}>
             <Component {...pageProps} />
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,10 +5,9 @@ import {usePageProperties} from "../hooks/page-properties";
 import {useEffect} from "react/cjs/react.production.min";
 import {useRouter} from "next/router";
 import {publicRoutes} from "../routes";
+import {route} from "next/dist/next-server/server/router";
 
 export default function Home() {
-
-
     return (
         <>
             <div className="text-center">
@@ -18,36 +17,51 @@ export default function Home() {
                         className="block text-green-600 xl:inline">{' '}in Salzgitter und Umgebung</span>
                 </h1>
                 <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatπat commodo. Elit sunt
-                    amet fugiat veniam occaecat fugiat aliqua.
+                    Ob Waldführung für Schulklassen, ein Waldgeburtstag für Kinder oder Waldbaden für Sie und Ihre Freunde,
+                    informieren Sie sich auf meiner Seite und fragen Sie noch heute ein tollen Erlebnis an.
                 </p>
             </div>
 
             <div className={'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-12'}>
                 {
                     [
-                        {img: 'https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80', title: 'Kindergeburtstag'},
-                        {img: 'https://images.unsplash.com/photo-1501276098931-5d481df52e60?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80', title: 'Schule'},
-                        {img: 'https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80', title: 'Waldbaden'},
+                        {
+                            img: 'https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80',
+                            title: 'Kindergeburtstag',
+                            page: 'geburtstag'
+                        },
+                        {
+                            img: 'https://images.unsplash.com/photo-1501276098931-5d481df52e60?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+                            title: 'Schule',
+                            page: 'schule'
+                        },
+                        {
+                            img: 'https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80',
+                            title: 'Waldbaden',
+                            page: 'waldbaden'
+                        },
                     ]
                         .map((value, index) => {
-                            return <button
-                                onClick={() => console.log('Clicked ' + index)}
-                                className={'col-span-1 shadow rounded-lg m-2 text-center bg-coolGray-50 hover:bg-coolGray-100'}>
-                                <div>
-                                    <img className={'rounded-t-lg w-full object-cover h-48'} src={value.img}
-                                         alt={'Bild x'}/>
-                                </div>
-                                <div className={'p-3'}>
-                                    <div className={'text-green-600 font-semibold'}>
-                                        {value.title}
+                            return <Link href={'/angebote/' + value.page}>
+                                <a
+                                    className={'col-span-1 shadow rounded-lg m-2 text-center bg-coolGray-50 hover:bg-coolGray-100'}>
+                                    <div>
+                                        <img className={'rounded-t-lg w-full object-cover h-48'} src={value.img}
+                                             alt={'Bild x'}/>
                                     </div>
-                                    <div className={'text-left text-gray-600 border-t border-gray-200 mt-1.5 pt-1.5'}>
-                                        Accusantium perferendis quis voluptates odit soluta excepturi est. At quibusdam quo voluptates possimus eius atque. Amet quia rerum enim.
-                                        {index}
+                                    <div className={'p-3'}>
+                                        <div className={'text-green-600 font-semibold'}>
+                                            {value.title}
+                                        </div>
+                                        <div
+                                            className={'text-left text-gray-600 border-t border-gray-200 mt-1.5 pt-1.5'}>
+                                            Accusantium perferendis quis voluptates odit soluta excepturi est. At
+                                            quibusdam quo voluptates possimus eius atque. Amet quia rerum enim.
+                                            {index}
+                                        </div>
                                     </div>
-                                </div>
-                            </button>
+                                </a>
+                            </Link>
                         })
                 }
             </div>
