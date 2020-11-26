36b53d9f63c2ee0bb683b45f19f402902b10bff1
Ruben <r.vitt@fme.de>
2020-11-26 07:03:31 +0100

    :sparkles:(general): some additions

---
+++ pages/angebote/waldbaden.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import {usePageProperties} from "../../hooks/page-properties";
+import {useEffect} from "react/cjs/react.production.min";
+import {useRouter} from "next/router";
+import {publicRoutes} from "../../routes";
+
+export default function Home() {
+
+
+    return (
+        <>
+            Hier ist ein Text über Waldbaden
+        </>
+    )
+}
--- routes.js
+++ routes.js
@@ -1,6 +1,12 @@
 export const publicRoutes = [
     {title: 'Startseite', path: '/', hideTitle: true},
-    {title: 'Waldgeburtstage', path: '/waldgeburtstag'},
-    {title: 'Schule', path: '/schule'},
+    {title: 'Waldgeburtstage', path: '/angebote/geburtstag'},
+    {title: 'Schule', path: '/angebote/schule'},
     {title: 'Kontakt', path: '/kontakt'}
 ]
+
+export const privateRoutes = [
+    {title: 'Waldbaden', path: '/angebote/waldbaden'}
+]
+
+export const allRoutes = [...publicRoutes, ...privateRoutes];
