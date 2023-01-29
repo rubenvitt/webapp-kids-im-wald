12d2f8399e2c8dfa6ad8c7839f080cdc845b1c6b
Ruben Vitt <dev@rubeen.one>
2023-01-29 14:09:10 +0100

    feat(nav): change mail link

--- components/molecules/MailLink.tsx
+++ components/molecules/MailLink.tsx
@@ -1,13 +1,10 @@
-'use client';
 import { LetterIcon } from '../../assets/svg';
 import Link from 'next/link';
 import React from 'react';
-import { useEmailAdress } from '../../hooks/email';
 
 export function MailLink() {
-    const mail = useEmailAdress();
     return (
-        <Link href={mail()} className="text-sm font-semibold leading-6 text-white hover:text-primary-100">
+        <Link href={'#contact'} className="text-sm font-semibold leading-6 text-white hover:text-primary-100">
             <LetterIcon className="inline mr-1 h-5 w-5" aria-hidden="true" /> Kontakt
         </Link>
     );
