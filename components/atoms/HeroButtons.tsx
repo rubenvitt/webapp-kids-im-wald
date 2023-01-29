3a2b8c57967c922f454d5d6edb0bd83de54f617a
Ruben Vitt <dev@rubeen.one>
2023-01-29 14:48:08 +0100

    feat(links): change links

--- components/atoms/HeroButtons.tsx
+++ components/atoms/HeroButtons.tsx
@@ -4,12 +4,12 @@ export function HeroButtons() {
     return (
         <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
-                href="#"
+                href="/#contact"
                 className="rounded-md bg-primary-700 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-primary-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-700"
             >
                 Jetzt buchen
             </a>
-            <a href="#" className="text-base font-semibold leading-7 text-white">
+            <a href="/infos" className="text-base font-semibold leading-7 text-white">
                 Weitere Informationen <span aria-hidden="true">→</span>
             </a>
         </div>
