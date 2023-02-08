f12c56372cdd5346e52645ffafc7a209288f3123
Ruben Vitt <dev@rubeen.one>
2023-02-08 16:03:41 +0100

    feat(links): change a to link

--- components/atoms/NavigationLink.tsx
+++ components/atoms/NavigationLink.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NavigationItem } from '../../types/navigation';
 import classNames from 'classnames';
+import Link from 'next/link';
 
 export function NavigationLink({ item, className }: { item: NavigationItem; className?: string }) {
     return (
-        <a key={item.name} href={item.href} className={classNames(`text-sm font-semibold leading-6`, className)}>
+        <Link key={item.name} href={item.href} className={classNames(`text-sm font-semibold leading-6`, className)}>
             {item.name}
-        </a>
+        </Link>
     );
 }
