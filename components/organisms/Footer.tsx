c064a285045160a188234ae2c878968ff892bd4e
Ruben Vitt <dev@rubeen.one>
2023-02-10 13:42:22 +0100

    feat(footer): add creator

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import Link from 'next/link';
 
 export function Footer() {
     return (
@@ -6,7 +7,11 @@ export function Footer() {
             <div className="mx-auto max-w-7xl py-12 px-6 md:flex md:items-center md:justify-between lg:px-8">
                 <div className="mt-8 md:order-1 md:mt-0">
                     <p className="text-center text-xs leading-5 text-gray-500">
-                        &copy; {moment().year()} Beate Vitt. Alle Rechte vorbehalten.
+                        &copy; {moment().year()} Beate Vitt. Alle Rechte vorbehalten. Geschrieben von{' '}
+                        <Link className="hover:text-orange-500" href="https://rubeen.one" target="_blank">
+                            Ruben Vitt 🐕
+                        </Link>
+                        .
                     </p>
                 </div>
             </div>
